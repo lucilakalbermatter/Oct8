47ce70f5f736b73760604d902b728646d74f88f6
lucilakalbermatter <lucilakalbermatter@hotmail.com>
2021-10-08 11:03:31 +0200

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Oct8
+
+#Find the Trap
+
+#Write a function that finds the word "trap" in the given string (not case sensitive). Return "BOO!!" if found, otherwise,"Relax, there's no trap.".
+Examples
+trap("There is a trap.") ➞ "BOO!!"
+
+trap("Hey, did you think there is a TRAP?") ➞ "BOO!!"
+
+trap("This goes traaa!!!") ➞ "Relax, there's no trap."
+Notes
+The string "trap" may appear in different character cases (i.e. uppercase, lowercase, mixed).
---
+++ src/main/java/Ex1.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(trap("This goes traaa"));
+
+
+    }
+
+    public static boolean trap (String sourceText){
+        //Compile the string to a regex Pattern
+        Pattern p = Pattern.compile("trap");
+
+        //Create a matcher for the above input
+        Matcher m = p.matcher(sourceText.toLowerCase());
+        boolean result = m.find();
+        if (result) {
+            System.out.println("BOO!!");
+        } else {
+            System.out.println("Relax, there is no trap");
+        }
+    return result;
+    }
+
+}
